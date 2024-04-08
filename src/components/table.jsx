7664ce21c2a96fb8276b246f185deadf2e797eb7
nilpeep <niluferk038@gmail.com>
2024-04-08 23:46:47 +0300

    table style

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Table = () => {
   return (
-    <div className='w-[1000px] h-[1000px] bg-blue-500 '> 
+    <div className='w-[1000px] h-[1000px] bg-blue-500 mx-auto my-5'> 
     Table
     </div>
   )
