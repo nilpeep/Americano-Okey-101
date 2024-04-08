3c308258d35032fd351a2556de31aa2b4a5b013c
nilpeep <niluferk038@gmail.com>
2024-04-08 23:58:53 +0300

    table reducers

--- src/store/table/index.js
+++ src/store/table/index.js
@@ -1,9 +1,15 @@
 import {createSlice} from "@reduxjs/toolkit"
 
 const initialState = {
+    players:[],
+    activePlayer:null
 }
 
 const tableSlice = createSlice({
     name: "table",
-    initialState
+    initialState,
+    reducers: {
+        setTable(state, action) {
+            state.table = action.payload
+        }}
 })
