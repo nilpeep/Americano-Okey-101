a55a228d7eb423c499f0606f0f07af32d70eef3b
nilpeep <niluferk038@gmail.com>
2024-04-08 23:49:56 +0300

    constants

---
+++ src/components/consts.js
@@ -0,0 +1,9 @@
+export const PLAYER_COUNT = 4
+
+export const TILE_COUNT = 106
+
+export const TILE_LINEUP  = 7
+
+export const TILE_LINEUP_COUNT = TILE_LINEUP * PLAYER_COUNT
+
+export const PLAYER_TILE_COUNT = 14
