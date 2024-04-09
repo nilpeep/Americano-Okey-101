63f680a0dbc3f23684daf4edf4bc5c8d0c189d78
nilpeep <niluferk038@gmail.com>
2024-04-09 23:46:10 +0300

    waiting for x more players

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useTable } from '../store/table/hooks'
+import { PLAYER_COUNT } from './consts'
+
+
 
 
 
@@ -8,6 +11,12 @@ export const Table = () => {
   const {players} = useTable()
   return (
     <div className='w-[1000px] h-[1000px] bg-blue-500 mx-auto my-5'> 
+    {
+      PLAYER_COUNT !== players.length && (
+        <div className='text-center text-white'>
+          <p>Waiting for {PLAYER_COUNT - players.length} more players</p>
+        </div>)
+    }
     {JSON.stringify(players, null,2)}
     </div>
   )
