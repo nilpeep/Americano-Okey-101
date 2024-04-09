12ddb85b8e05bdee941434943a9989bb479354c2
nilpeep <niluferk038@gmail.com>
2024-04-09 23:49:20 +0300

    styling

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -10,11 +10,11 @@ export const Table = () => {
 
   const {players} = useTable()
   return (
-    <div className='w-[1000px] h-[1000px] bg-blue-500 mx-auto my-5'> 
+    <div className='w-[1000px] h-[1000px] bg-blue-500 mx-auto my-5 relative'> 
     {
       PLAYER_COUNT !== players.length && (
-        <div className='text-center text-white'>
-          <p>Waiting for {PLAYER_COUNT - players.length} more players</p>
+        <div className='text-center text-white absolute top-0 left-0 px-4 h-8 text-sm bg-orange-500 rounded-br'>
+          <p>Waiting for <b className='mx-0.5'>{PLAYER_COUNT - players.length}</b>  more players</p>
         </div>)
     }
     {JSON.stringify(players, null,2)}
